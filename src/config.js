2788c1031995bc872a2696b6f44f3d5c38b663d3
kdmricsegarciachristian@gmail.com <kdmricsegarciachristian@gmail.com>
2026-07-13 20:01:35 -0500

    Actualizar URL de backend de produccion en config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.PROD 
-  ? 'https://unireserva-backend-christian.azurewebsites.net' 
+  ? 'https://unireserva-backend-christian-cch0fxe3crf0h8bd.canadacentral-01.azurewebsites.net' 
   : 'http://localhost:5000';
